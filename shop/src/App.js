7b00d558eb3fe4697f5265da6ea33f69c8558193
Jova063 <javlonxolmurodov2000@gmail.com>
2022-06-30 18:10:57 +0500

    sd

---
+++ shop/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import '../src/App.css'
+import { BsTelephone} from 'react-icons/bs'
+import {ImLocation} from 'react-icons/im'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       <navbar className='navbar'>
+             <div className='navbar_top'>
+                <BsTelephone/>
+                <p className='tel_nomber'>+998 (99) 000 00 00</p>
+                <ImLocation/>
+                <p className='tel_location'>Ташкент</p>
+             </div>
+       </navbar>
+      </div>
+    )
+  }
+}
