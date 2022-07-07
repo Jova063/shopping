83d0c13a727e3b89bcf7e04d031d44b20a295d96
Jova063 <javlonxolmurodov2000@gmail.com>
2022-07-07 17:59:54 +0500

    cd
    cd

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import '../src/App.css'
 import Navbar from './pages/js/Navbar'
 import img from "./pages/img/kamera.png"
+import img1 from "./pages/img/yangi.png"
+import img2 from "./pages/img/klipartz.jpg"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Carousel} from "react-bootstrap"
+import {MdAddShoppingCart} from "react-icons/md"
+const data=require("./pages/js/new") 
+const foto=require("./pages/js/foto")
 export default class App extends Component {
   render() {
     return (
@@ -48,7 +53,110 @@ export default class App extends Component {
         <div className='news_top'>
           <h1 className='news_text'>Новинки </h1>
           <a href='#!' className='news_link'>Все категории</a>
-        </div>
+           </div>
+          <div className='card_wrapper'>
+            {data.map(item=>{
+              return <div className='card_list'>
+                <img src={img1} alt="" className='card_img'/>
+                <div className='card_text'>
+                    <p className='card_title'>{item.title}</p>
+                    <p className='card_sum'>{item.sum}</p>
+                    <p className='card_skit'>{item.skit}</p>
+                    <div className='card_button'>
+                       <select className='card_drop'>
+                          <option>1 шт</option>
+                          <option>2 шт</option>
+                          <option>3 шт</option>
+                       </select>
+                       <div className='card_icons'>
+                         <MdAddShoppingCart className="card_icon"/>
+                       </div>
+                    </div>
+                </div>
+              </div>
+            })}
+            
+          </div>
+       
+      </section>
+
+      <section className='skit'>
+        <div className='skit_top'>
+          <h1 className='skit_text'>Новинки <span className='skit_text_span'>%</span> </h1>
+          <a href='#!' className='skit_link'>Все товары в категории </a>
+           </div>
+          <div className='card_wrapper'>
+            {data.map(item=>{
+              return <div className='card_list'>
+                <img src={img1} alt="" className='card_img'/>
+                <div className='card_text'>
+                <span className='skit_text_span'>12%</span>
+                    <p className='card_title'>{item.title}</p>
+                    <p className='card_sum'>{item.sum}</p>
+                    <p className='card_skit'>{item.skit}</p>
+                    <div className='card_button'>
+                       <select className='card_drop'>
+                          <option>1 шт</option>
+                          <option>2 шт</option>
+                          <option>3 шт</option>
+                       </select>
+                       <div className='card_icons'>
+                         <MdAddShoppingCart className="card_icon"/>
+                       </div>
+                    </div>
+                </div>
+              </div>
+            })}
+            
+          </div>
+       
+      </section>
+      <section className='top'>
+        <div className='top_top'>
+          <h1 className='top_text'>Популярные категории </h1>
+          <a href='#!' className='top_link'>Все товары в категории </a>
+           </div>
+          <div className='card_wrapper1'>
+            {foto.map(item=>{
+              return <div className='card_list1'>
+                <img src={img2} alt="" className='card_img1'/>               
+                    <p className='card_title1'>{item.title}</p>
+              </div>
+            })}
+            
+          </div>
+       
+      </section>
+      <section className='skit'>
+        <div className='skit_top'>
+          <h1 className='skit_text'>Новинки <span className='skit_text_span'>%</span> </h1>
+          <a href='#!' className='skit_link'>Все товары в категории </a>
+           </div>
+          <div className='card_wrapper'>
+            {data.map(item=>{
+              return <div className='card_list'>
+                <img src={img1} alt="" className='card_img'/>
+                <div className='card_text'>
+                <span className='skit_text_span'>12%</span>
+                    <p className='card_title'>{item.title}</p>
+                    <p className='card_sum'>{item.sum}</p>
+                    <p className='card_skit'>{item.skit}</p>
+                    <div className='card_button'>
+                       <select className='card_drop'>
+                          <option>1 шт</option>
+                          <option>2 шт</option>
+                          <option>3 шт</option>
+                       </select>
+                       <div className='card_icons'>
+                         <MdAddShoppingCart className="card_icon"/>
+                       </div>
+                    </div>
+                </div>
+              </div>
+            })}
+            
+          </div>
+       
       </section>
        
       </div>
---
+++ shop/src/pages/js/foto.js
@@ -0,0 +1,44 @@
+const foto =[
+    {
+        id:1,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:2,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:3,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:4,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:5,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:6,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:7,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:8,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:9,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }, {
+        id:10,
+        img: "../img/klipartz.jpg",
+        title: "Фотоаппараты",
+    }
+]
+module.exports=foto
